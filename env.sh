7a422d6edc08860efd67e1abe9bf6516ad833630
Alex Makunin <alex.makunin@gmail.com>
2019-02-21 13:13:10 +0000

    source env.sh from anywhere
    Resolves #36

--- env.sh
+++ env.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# set path to repository
+repoDir="$( cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd -P )"
+
 # add miniconda to the path
-export PATH=$(pwd)/binder/deps/conda/bin:$PATH
+export PATH=${repoDir}/binder/deps/conda/bin:$PATH
 
 # add texlive to the path
 if [ "$(uname)" == "Darwin" ]; then
-    export PATH=$(pwd)/binder/deps/texlive/bin/x86_64-darwin:$PATH
+    export PATH=${repoDir}/binder/deps/texlive/bin/x86_64-darwin:$PATH
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    export PATH=$(pwd)/binder/deps/texlive/bin/x86_64-linux:$PATH
+    export PATH=${repoDir}/binder/deps/texlive/bin/x86_64-linux:$PATH
 fi
 
-# determine name of current directory, use as conda environment name
-CONDANAME=${PWD##*/}
+# determine name of repository directory, use as conda environment name
+CONDANAME=${repoDir##*/}
 
 # activate conda environment
 source activate $CONDANAME
