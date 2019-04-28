76a95cc05d12513e900059e2e972f6298e8e830e
Alistair Miles <alimanfoo@googlemail.com>
2019-04-28 12:35:01 +0100

    add support for variables; update conda

--- install-conda.sh
+++ install-conda.sh
@@ -10,15 +10,30 @@ set -e
 
 # conda setup
 CONDADIR=conda
+
+# conda environment name - use name of current directory
 CONDANAME=${PWD##*/}
 
-# descend into dependencies directory
-DEPSDIR=binder/deps
-mkdir -pv $DEPSDIR
-cd $DEPSDIR
+# remember directory where this script was run from
+REPODIR=${PWD}
 
-# put dependencies on the path
-export PATH=./${CONDADIR}/bin:$PATH
+# determine directory in which conda will be installed
+if [[ -z "${MALARIAGEN_BINDER_INSTALLDIR}" ]]; then
+    # if not specified by user, default installation location
+    INSTALLDIR=${REPODIR}/binder/deps
+else
+    # allow user to specify installation location
+    INSTALLDIR=${MALARIAGEN_BINDER_INSTALLDIR}
+fi
+
+# ensure installation directory exists
+mkdir -pv $INSTALLDIR
+
+# change into into installation directory
+cd $INSTALLDIR
+
+# put conda on the path
+export PATH=${INSTALLDIR}/${CONDADIR}/bin:$PATH
 
 # install miniconda
 if [ ! -f miniconda.installed ]; then
@@ -28,7 +43,7 @@ if [ ! -f miniconda.installed ]; then
     rm -rf $CONDADIR
 
     if [ "$(uname)" == "Darwin" ]; then
-        # Install for Mac OS X platform        
+        # Install for Mac OS X platform
         # download miniconda
         curl --continue-at - --remote-name https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
 
@@ -59,7 +74,11 @@ else
     echo "[install] skipping miniconda installation"
 fi
 
+# return to original location
+cd $REPODIR
+
 echo "[install] installing packages"
+
 # ensure channel order - cannot rely on environment.yml
 # https://github.com/conda/conda/issues/7238
 conda config --add channels pyviz/label/dev
@@ -67,7 +86,12 @@ conda config --add channels bokeh/label/dev
 conda config --add channels intake
 conda config --add channels bioconda
 conda config --add channels conda-forge
+
+# ensure conda is up to date
+conda update --yes conda
+
 # install packages
-conda env update --name $CONDANAME --file ../environment.yml
+conda env update --name $CONDANAME --file ./binder/environment.yml
+
 # clean conda caches
 conda clean --yes --all
